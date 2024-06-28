ace30b3d31d0374a1511268feb6938f5b2d950f9
Artemii <latovgor@gmail.com>
2024-06-28 12:30:52 +0300

    mess2

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,2 +1,12 @@
 /// <reference types="vite/client" />
 /// <reference types="vite-plugin-svgr/client" />
+
+
+// interface ImportMetaEnv {
+//     readonly VITE_LOCAL_USER_ID: number
+//     readonly VITE_BOT_NAME: string
+//   }
+  
+//   interface ImportMeta {
+//     readonly env: ImportMetaEnv
+//   }
